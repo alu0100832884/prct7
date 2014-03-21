79a85c2f5919a284f2984b6ba1b5370f2971b2f8
SARA <alu0100832884@student05.aulas.priv>
2014-03-21 10:43:34 +0000

    practica terminada

--- src/prog2.py
+++ src/prog2.py
@@ -1,6 +1,10 @@
 import prog1
 
-valores = (10,100,1000,10000,100000,1000000,10000000)
+valores = (1e1,1e2,1e3,1e4,1e5,1e6)
 for valor in valores:
   y = prog1.aproximacion(valor)
   print y
+  #el numero maximo de elementos de u-pla es un numero muy elevado finito
+  #se producen errodes de memoria para los elementos iguales o mayores que 1e7.
+  #si se pueden definir los elementus de la t-upla en notacion cientifica
+  #pyc es el archibo que se crea al importar cualquier variable
